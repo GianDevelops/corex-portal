f753fb9d24a1035e4b5bd02efa5a056bf536d561
GianDevelops <gian.jacome@gmail.com>
2025-07-13 18:29:42 -0400

    Update strcuture
    Trying previous update again because it didnt work

--- src/App.js
+++ src/App.js
@@ -454,7 +454,7 @@ const Portal = ({ user, setNotification }) => {
                 </div>
             </header>
             <main className="flex-1 flex flex-col min-h-0">
-                <div className="max-w-7xl w-full mx-auto p-4 md:p-8 flex-1 flex flex-col">
+                <div className="max-w-7xl w-full mx-auto p-4 md:p-8 flex flex-col flex-1">
                     {user.role === 'designer' && (
                         <div className="mb-6 flex justify-between items-center flex-shrink-0">
                              <div className="flex items-center gap-2 bg-gray-200 p-1 rounded-lg">
